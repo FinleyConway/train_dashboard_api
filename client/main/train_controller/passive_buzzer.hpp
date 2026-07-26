105d2c2b22dbd17f4a04d05dbdffe846ebe41046
FinleyConway <finleyconway03@gmail.com>
2026-07-26 23:33:41 +0100

    Interesting design pattern for replacing two phase initialisation

--- client/main/train_controller/passive_buzzer.hpp
+++ client/main/train_controller/passive_buzzer.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <expected>
+
+#include <esp_err.h>
 #include <driver/ledc.h>
 #include <driver/gpio.h>
 
@@ -8,14 +11,7 @@
 namespace client {
     class passive_buzzer_t {
     public:
-        passive_buzzer_t() = default;
-        ~passive_buzzer_t() {
-            ledc_stop(c_pwm_mode, m_pwm_channel, 0);
-        }
-
-        void init(gpio_num_t pwm, ledc_channel_t pwm_channel) {
-            m_pwm_channel = pwm_channel;
-
+        static std::expected<passive_buzzer_t, esp_err_t> create(gpio_num_t pwm, ledc_channel_t pwm_channel) {
             // configure pwm timer
             ledc_timer_config_t pwm_timer = {};
             pwm_timer.speed_mode      = c_pwm_mode;
@@ -23,7 +19,10 @@ namespace client {
             pwm_timer.timer_num       = c_timer_num;
             pwm_timer.freq_hz         = 1000;
             pwm_timer.clk_cfg         = LEDC_AUTO_CLK;
-            ESP_ERROR_CHECK(ledc_timer_config(&pwm_timer));
+            
+            if (auto err = ledc_timer_config(&pwm_timer); err != ESP_OK) {
+                return std::unexpected(err);
+            }
 
             ledc_channel_config_t cfg = {};
             cfg.gpio_num   = pwm;
@@ -32,24 +31,29 @@ namespace client {
             cfg.timer_sel  = c_timer_num;
             cfg.duty       = c_max_duty / 2;
             cfg.hpoint     = 0;
-            ESP_ERROR_CHECK(ledc_channel_config(&cfg));
 
-            m_initialised = true;
-        }
-
-        esp_err_t stop() {
-            if (!m_initialised) {
-                return ESP_ERR_INVALID_STATE;
+            if (auto err = ledc_channel_config(&cfg); err != ESP_OK) {
+                return std::unexpected(err);
             }
 
+            return passive_buzzer_t(pwm_channel);
+        }
+
+        ~passive_buzzer_t() {
+            ledc_stop(c_pwm_mode, m_pwm_channel, 0);
+        }
+
+        passive_buzzer_t(const passive_buzzer_t&) = delete;
+        passive_buzzer_t& operator=(const passive_buzzer_t&) = delete;
+
+        passive_buzzer_t(passive_buzzer_t&&) = default;
+        passive_buzzer_t& operator=(passive_buzzer_t&&) = default;
+
+        esp_err_t stop() {
             return ledc_stop(c_pwm_mode, m_pwm_channel, 0);
         }
 
         esp_err_t set_tone(uint32_t frequency) {
-            if (!m_initialised) {
-                return ESP_ERR_INVALID_STATE;
-            }
-
             if (frequency == 0) {
                 return stop();
             }
@@ -69,7 +73,10 @@ namespace client {
         }
 
     private:
-        bool m_initialised = false;
+        explicit passive_buzzer_t(ledc_channel_t pwm_channel) : m_pwm_channel(pwm_channel) {
+        }
+
+    private:
         ledc_channel_t m_pwm_channel;
 
         static constexpr ledc_mode_t c_pwm_mode = LEDC_LOW_SPEED_MODE;
